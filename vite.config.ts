b64fdda5b45b0cda847fd5c5c76cae02269feeb2
Lovejeet Singh <maheshnabha190@gmail.com>
2025-10-30 18:41:05 -0400

    Removed unused .bolt folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // This is the correct setting for your repo
-  base: '/', 
+  base: '/version-2/', 
   plugins: [react()],
-})
+}) 
